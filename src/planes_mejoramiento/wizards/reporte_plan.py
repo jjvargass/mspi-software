2918c1036090b8c81f8bcf28b6071ee27961518a
jotavargas <jotavargas89@gmail.com>
2018-10-02 14:59:39 -0500

    funcion para data de reporte plan

--- src/planes_mejoramiento/wizards/reporte_plan.py
+++ src/planes_mejoramiento/wizards/reporte_plan.py
@@ -40,38 +40,113 @@ class plan_mejoramientos_wizard_reporte_plan(models.TransientModel):
     # methods
     # -------------------
 
+    def definir_plan(self, dic_plan, obj_plan={}):
+        if obj_plan:
+            dic_plan['plan_id'] = obj_plan.id or ''
+            dic_plan['plan_fecha'] = obj_plan.fecha or ''
+            dic_plan['plan_origen'] = obj_plan.origen_id.name or ''
+            dic_plan['plan_sub_origen'] = obj_plan.sub_origen_id.name or ''
+            dic_plan['plan_unidad'] = obj_plan.dependencia_id.name or ''
+            dic_plan['plan_usuario'] = obj_plan.user_id.name or ''
+            dic_plan['plan_radicado'] = obj_plan.radicado or ''
+        else:
+            dic_plan['plan_id'] = ''
+            dic_plan['plan_fecha'] = ''
+            dic_plan['plan_origen'] = ''
+            dic_plan['plan_sub_origen'] = ''
+            dic_plan['plan_unidad'] = ''
+            dic_plan['plan_usuario'] = ''
+            dic_plan['plan_radicado'] = ''
+
+    def definir_hallazgo(self, dic_hallazgo, obj_hallazgo={}):
+        if obj_hallazgo:
+            dic_hallazgo['hallazgo_nombre'] = obj_hallazgo.name or ''
+            dic_hallazgo['hallazgo_nombre'] = obj_hallazgo.name or ''
+            dic_hallazgo['hallazgo_unidad'] = obj_hallazgo.dependencia_id.name or ''
+        else:
+            dic_hallazgo['hallazgo_nombre'] = ''
+            dic_hallazgo['hallazgo_nombre'] = ''
+            dic_hallazgo['hallazgo_unidad'] = ''
+
+    def definir_accion(self, dic_accion, obj_accion={}):
+        if obj_accion:
+            dic_accion['accion_descripcion'] = obj_accion.descripcion or ''
+            dic_accion['accion_tipo'] = obj_accion.tipo or ''
+            dic_accion['accion_objetivo'] = obj_accion.objetivo or ''
+            dic_accion['accion_indicador'] = obj_accion.indicador or ''
+            dic_accion['accion_meta'] = obj_accion.meta or ''
+            dic_accion['accion_unidad_medida'] = obj_accion.unidad_medida or ''
+            dic_accion['accion_unidad'] = obj_accion.dependencia_id.name or ''
+            dic_accion['accion_recursos'] = 'muchos' # muchos
+            dic_accion['accion_fecha_inicio'] = obj_accion.fecha_inicio or ''
+            dic_accion['accion_fecha_fin'] = obj_accion.fecha_inicio or ''
+            dic_accion['accion_fecha_estado'] = obj_accion.state or ''
+        else:
+            dic_accion['accion_descripcion'] = ''
+            dic_accion['accion_tipo'] = ''
+            dic_accion['accion_objetivo'] = ''
+            dic_accion['accion_indicador'] = ''
+            dic_accion['accion_meta'] = ''
+            dic_accion['accion_unidad_medida'] = ''
+            dic_accion['accion_unidad'] = ''
+            dic_accion['accion_recursos'] = 'muchos' # muchos
+            dic_accion['accion_fecha_inicio'] = ''
+            dic_accion['accion_fecha_fin'] = ''
+            dic_accion['accion_fecha_estado'] = ''
+
+    def definir_avance(self, dic_avance, obj_avance={}):
+        if obj_avance:
+            dic_avance['avance_descripcion'] = obj_avance.descripcion or ''
+            dic_avance['avance_state'] = obj_avance.state or ''
+            dic_avance['avance_calificacion'] = obj_avance.tipo_calificacion_id.name or ''
+            dic_avance['avance_porcentaje_avance'] = obj_avance.porcentaje_avance or ''
+            dic_avance['avance_observacion'] = obj_avance.observacion or ''
+            dic_avance['avance_fecha_creacion'] = obj_avance.fecha_creacion or ''
+        else:
+            dic_avance['avance_descripcion'] = ''
+            dic_avance['avance_state'] = ''
+            dic_avance['avance_calificacion'] = ''
+            dic_avance['avance_porcentaje_avance'] = ''
+            dic_avance['avance_observacion'] = ''
+            dic_avance['avance_fecha_creacion'] = ''
+
     @api.multi
     def crear_reporte_plan(self):
         all_data = []
-        plan = {}
         # construir diccionario
-        plan['plan_id'] = self.plan_id.id or ''
-        plan['plan_fecha'] = self.plan_id.fecha or ''
-        plan['plan_origen'] = self.plan_id.origen_id.name or ''
-        plan['plan_sub_origen'] = self.plan_id.sub_origen_id.name or ''
-        plan['plan_unidad'] = self.plan_id.dependencia_id.name or ''
-        plan['plan_usuario'] = self.plan_id.user_id.name or ''
-        plan['plan_radicado'] = self.plan_id.radicado or ''
         if (self.plan_id.hallazgo_ids):
             for hallazgo in self.plan_id.hallazgo_ids:
-                plan['hallazgo_nombre'] = self.hallazgo.name or ''
-                plan['hallazgo_nombre'] = self.hallazgo.name or ''
-                plan['hallazgo_unidad'] = self.hallazgo.dependencia_id or ''
+                plan = {}
+                print hallazgo.name
                 if (hallazgo.acciones_ids):
                     for accion in hallazgo.acciones_ids:
-                        plan['accion_descripcion'] = self.accion.descripcion or ''
+                        print accion.name
+                        if(accion.avance_ids):
+                            for avance in accion.avance_ids:
+                                self.definir_avance(dic_avance, obj_avance)
+                        else:
+                            self.definir_plan(plan, self.plan_id)
+                            self.definir_hallazgo(plan, hallazgo)
+                            self.definir_accion(plan, accion)
+                            self.definir_avance(plan)
+                            all_data.append(plan)
                 else:
-                    print "vacio accion"
-                    print "vacio avance"
+                    self.definir_plan(plan, self.plan_id)
+                    self.definir_hallazgo(plan, hallazgo)
+                    self.definir_accion(plan)
+                    self.definir_avance(plan)
+                    all_data.append(plan)
         else:
-            print "vacio hallazgo"
-            print "vacio accion"
-            print "vacio avance"
+            self.definir_plan(plan, self.plan_id)
+            self.definir_hallazgo(plan)
+            self.definir_accion(plan)
+            self.definir_avance(plan)
+            all_data.append(plan)
 
         # crear reporte
         documento = reportes.crear_reporte(
             self,
-            proyecto,
+            all_data,
             'PLAN_INTERNO',
             'xls',
             'plan_interno.ods',
